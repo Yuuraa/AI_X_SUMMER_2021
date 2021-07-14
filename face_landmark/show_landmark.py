84aaab3918444f03fe93c284fa21807c9297e1be
Yuuraa <yoorachoi@gm.gist.ac.kr>
2021-07-14 17:54:07 +0900

    Enable showing important parts of face individually - jaw, mouth, nose, etc

--- face_landmark/show_landmark.py
+++ face_landmark/show_landmark.py
@@ -34,22 +34,24 @@ def draw_individual_detections(image, detector, predictor):
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
+        ### 아래 부분을 수정해 눈, 코, 입, 눈썹 중 원하는 부분만을 선택해 보세요
         for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+        ###
             clone = image.copy()
             cv2.putText(clone, name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-        for (x, y) in shape[i:j]:
-            cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
+            for (x, y) in shape[i:j]:
+                cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
 
-        (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+            (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
 
-        roi = image[y:y+h, x:x+w]
-        roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
+            roi = image[y:y+h, x:x+w]
+            roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
+            cv2.putText(roi, name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 2)
 
-        cv2.imshow("ROI", roi)
-        cv2.imshow("Image", clone)
-        cv2.waitKey(0)
+            cv2.imshow(f"{name.upper()}", roi)
 
+        # 얼굴의 특징들 - 눈, 코, 입, 턱선, 눈썹을 도형으로 색칠해줍니다
         output = face_utils.visualize_facial_landmarks(image, shape)
         cv2.imshow("Image", output)
         cv2.waitKey(0)
